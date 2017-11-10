e1244437a9feb006efe2c459c34976645d6a93e1
Obi <obinnawalker@gmail.com>
2017-11-10 09:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers){
+  let newDrivers= []
+  newDrivers.push(drivers[0])
+  newDrivers.push(drivers[1])
+  return newDrivers
+}
+
+const returnLastTwoDrivers = function (drivers){
+  let l= drivers.length
+  let newDrivers=[]
+  newDrivers.push(drivers[l-2])
+  newDrivers.push(drivers[l-1])
+  return newDrivers
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+let createFareMultiplier = function(intInput){
+  return function(fare){return fare*intInput}
+}
+const fareDoubler = function (fare){
+  return fare*2
+  
+}
